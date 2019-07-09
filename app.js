5e2126d535192eaf00c2fdd111ede6436b679183
chiheb ben jamaa <chihabbenjamaa04@gmail.com>
2019-07-09 12:00:19 +0200

    chnage the post

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.listen(PORT, function () {
 */
 
 //define post for deployment prupoes
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log("App is running on port " + port);
   });
