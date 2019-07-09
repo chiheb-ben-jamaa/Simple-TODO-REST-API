df8035951ff190816baeb33fb2f62f5901dae66b
chiheb ben jamaa <chihabbenjamaa04@gmail.com>
2019-07-09 12:29:38 +0200

    make some chaniging fix some iusse in heroku depoyment

--- app.js
+++ app.js
@@ -25,9 +25,10 @@ app.listen(PORT, function () {
 */
 
 //define post for deployment prupoes
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+var port = process.env.PORT || 8000;
+server.listen(port, function() {
+    console.log("App is running on port " + port);
+});
 
 
 
