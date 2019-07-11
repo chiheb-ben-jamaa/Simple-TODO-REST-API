30efffb51c749e86ded0061b95e8b1eaca6e3c24
chiheb ben jamaa <chihabbenjamaa04@gmail.com>
2019-07-11 11:28:58 +0200

    master

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ app.use(morgan('combined'))
 
 
 
-app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(express.static(path.join(__dirname, 'public')));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
